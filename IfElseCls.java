f487fa59684d959ec7156b08deee942e1632a847
urusuma <sunroseuma@gmail.com>
2022-05-10 09:16:35 -0700

    first commit

---
+++ IfElseCls.java
@@ -0,0 +1,16 @@
+package sampleproject;
+
+public class IfElseCls {
+
+	public static void main(String[] args) {
+		 int x = 8;
+			 if(x % 2 == 0){
+			 System.out.println("Even number");
+			   }
+			 else{
+			 System.out.println("Odd Number");
+			   }
+
+	}
+
+}
---
+++ sampro.java
@@ -0,0 +1,9 @@
+package sampleproject;
+
+public class sampro {
+
+	public static void main(String[] args) {
+	System.out.println("Hello, Welcome World");
+	}
+
+}
